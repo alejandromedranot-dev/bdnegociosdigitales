c8a744de13c2cbd33d810bdf0255bd7b88161059
Jose Luis Herrera Gallardo <gallardo@MacBook-Pro-de-Jose.local>
2026-03-24 18:20:57 -0600

    Store procedures terminado!

--- 02-ManipulacionAvanzada/03-programacion-sql/02-stored-procedures-transact-sql.sql
+++ 02-ManipulacionAvanzada/03-programacion-sql/02-stored-procedures-transact-sql.sql
@@ -147,12 +147,394 @@ WHERE EXISTS(
 SELECT 1
 FROM Customers 
 WHERE CustomerID = 'ANTONi');
-
+GO
 -- Ejercicios: crear un SP que reciba un número y que verifique que no sea negativo, 
 -- si es negativo imprimir valor no valido, y sino multiplicarlo por cinco y mostrarlo 
 -- para mostrar usar un select
 
+CREATE OR ALTER PROCEDURE usp_numero_multiplicar 
+@number INT 
+AS
+BEGIN
+   IF @number<=0
+   BEGIN
+     PRINT 'El número no puede ser negativo ni cero'
+     RETURN;
+   END
+
+   SELECT (@number * 5) AS [OPERACIÓN]
+END;
+GO
+
+EXEC usp_numero_multiplicar -34;
+EXEC usp_numero_multiplicar 0;
+EXEC usp_numero_multiplicar 5;
+GO
+
 -- Ejercicio 2: Crear un sp que reciba un nombre y lo imprima en mayusculas
+CREATE OR ALTER PROC usp_nombre_mayusculas 
+@name VARCHAR(15)
+AS
+BEGIN
+   SELECT UPPER(@name) AS [NAME]
+END;
+GO
+
+EXEC usp_nombre_mayusculas 'Monico';
+GO
+/* ======================PARAMETROS DE SALIDA==========================*/
+
+CREATE OR ALTER PROC spu_numeros_sumar
+  @a INT, 
+  @b INT, 
+  @resultado INT OUTPUT
+  AS
+  BEGIN
+     SET @resultado = @a + @b
+  END;
+GO
+
+DECLARE @res INT;
+EXEC spu_numeros_sumar 5,7,@res OUTPUT;
+SELECT @res AS [Resultado];
+GO
+
+CREATE OR ALTER PROC spu_numeros_sumar2
+  @a INT, 
+  @b INT, 
+  @resultado INT OUTPUT
+  AS
+  BEGIN
+     SELECT @resultado = @a + @b
+  END;
+GO
+
+DECLARE @res INT;
+EXEC spu_numeros_sumar2 5,7,@res OUTPUT;
+SELECT @res AS [Resultado];
+GO
+-- CREAR UN SP QUE DEVUELVA EL AREA DE UN CIRCULO
+CREATE OR ALTER PROC usp_area_circulo 
+@radio DECIMAL(10,2),
+@area DECIMAL(10,2) OUTPUT
+AS
+BEGIN
+    --SET @area = PI() * @radio * @radio;
+    SET @area = PI() * POWER(@radio,2);
+END;
+GO
 
 
--- TODO: PARAMETROS DE SALIDA
+DECLARE @r DECIMAL(10,2);
+EXEC usp_area_circulo 2.4, @r OUTPUT;
+SELECT @r AS [area del circulo];
+GO
+
+-- crear un sp que reciba un idcliente y devuelva el nombre
+
+CREATE OR ALTER PROC spu_cliente_obtener
+  @id NCHAR(10),
+  @name NVARCHAR(40) OUTPUT
+AS 
+BEGIN
+  IF LEN(@id) = 5
+  BEGIN
+    IF EXISTS (SELECT 1 FROM CUSTOMERS WHERE CustomerID = @id)
+    BEGIN
+        SELECT @name = CompanyName
+        FROM Customers
+        WHERE CustomerID = @id;
+
+        RETURN;
+    END
+
+    PRINT 'EL CUSTOMER NO EXISTE';
+    RETURN;
+  END
+
+  PRINT 'EL ID DEBE SER DE TAMAÑO 5';
+END;
+GO
+
+SELECT * FROM Customers;
+
+
+DECLARE @name VARCHAR(40) 
+EXEC spu_cliente_obtener 'AROUX', @name OUTPUT
+SELECT @name AS [NOMBRE DEL CLIENTE];
+GO
+/*===========================CASE==============================*/
+
+CREATE OR ALTER PROC spu_Evaluar_Calificacion
+@calif INT
+AS 
+BEGIN
+    SELECT 
+      CASE 
+          WHEN @calif >= 90 THEN 'EXCELENTE'
+          WHEN @calif >= 70 THEN 'APROBADO'
+          WHEN @calif >= 60 THEN 'REGULAR'
+          ELSE 'NO ACREDITO'
+      END AS [RESULTADO];
+END;
+
+
+EXEC spu_Evaluar_Calificacion 100;
+EXEC spu_Evaluar_Calificacion 75;
+EXEC spu_Evaluar_Calificacion 55;
+EXEC spu_Evaluar_Calificacion 65;
+GO
+-- Case dentro de un select caso real
+use NORTHWND;
+
+CREATE TABLE bdstored.dbo.Productos
+(
+  nombre VARCHAR(50), 
+  precio money
+
+);
+
+-- Inserta los datos basados en la consulta (Select)
+INSERT INTO bdstored.dbo.Productos
+SELECT 
+  ProductName, UnitPrice 
+  FROM NORTHWND.dbo.Products;
+
+-- EJERCICIO CON CASE
+
+SELECT 
+  nombre, 
+  precio, 
+  CASE 
+      WHEN precio >= 200 THEN 'Caro' 
+      WHEN precio >= 100 THEN 'Medio'
+      ELSE 'Barato'
+  END AS [Categoria]
+FROM bdstored.dbo.Productos;
+GO
+
+-- SELECCIONA LOS CLIENTES, CON SU NOMBRE, PAIS, CIUDAD Y REGION (LOS VALORES NULOS, VISUALIZALOS CON LA LEYENDA SIN REGION), ADEMAS QUIERO QUE TODO EL RESULTADO ESTE MAYUSCULA
+
+use NORTHWND;
+GO
+
+CREATE OR ALTER view vw_buena
+AS
+SELECT 
+  UPPER(CompanyName) AS [CompanyName], 
+  UPPER(c.Country) AS [Country], 
+  UPPER(c.City) AS [City], 
+  UPPER(ISNULL(c.Region, 'Sin Region')) AS [RegionLimpia], 
+  LTRIM(UPPER(CONCAT(e.FirstName, ' ', e.LastName))) AS [FullName],
+  ROUND(SUM(od.Quantity * od.UnitPrice), 2) AS [Total],
+    CASE 
+       WHEN SUM(od.Quantity * od.UnitPrice) >=30000 AND SUM(od.Quantity * od.UnitPrice) <= 60000 THEN 'GOLD'
+       WHEN SUM(od.Quantity * od.UnitPrice) >=10000 AND SUM(od.Quantity * od.UnitPrice) < 30000 THEN 'SILVER'
+       ELSE 'BRONCE'
+    END AS [MEDALLON]   
+FROM NORTHWND.dbo.Customers as c
+INNER JOIN 
+NORTHWND.dbo.Orders AS o
+ON c.CustomerID = o.CustomerID
+INNER JOIN [Order Details] AS od
+ON o.OrderID = od.OrderID
+INNER JOIN Employees as e
+ON e.EmployeeID = o.EmployeeID
+GROUP BY c.CompanyName, c.Country, c.City, c.Region,CONCAT(e.FirstName, ' ', e.LastName);
+GO
+
+CREATE OR ALTER PROC spu_informe_clientes_empleados
+@nombre VARCHAR(50), 
+@region VARCHAR(50)
+AS
+BEGIN
+     SELECT * 
+        FROM vw_buena
+        WHERE FullName = @nombre 
+        AND RegionLimpia = @region;  
+END;
+
+
+EXEC spu_informe_clientes_empleados 'andrew Fuller', 'sin region';
+
+
+
+SELECT 
+  UPPER(CompanyName) AS [CompanyName], 
+  UPPER(c.Country) AS [Country], 
+  UPPER(c.City) AS [City], 
+  UPPER(ISNULL(c.Region, 'Sin Region')) AS [RegionLimpia], 
+  LTRIM(UPPER(CONCAT(e.FirstName, ' ', e.LastName))) AS [FullName],
+  ROUND(SUM(od.Quantity * od.UnitPrice), 2) AS [Total],
+    CASE 
+       WHEN SUM(od.Quantity * od.UnitPrice) >=30000 AND SUM(od.Quantity * od.UnitPrice) <= 60000 THEN 'GOLD'
+       WHEN SUM(od.Quantity * od.UnitPrice) >=10000 AND SUM(od.Quantity * od.UnitPrice) < 30000 THEN 'SILVER'
+       ELSE 'BRONCE'
+    END AS [MEDALLON]   
+FROM NORTHWND.dbo.Customers as c
+INNER JOIN 
+NORTHWND.dbo.Orders AS o
+ON c.CustomerID = o.CustomerID
+INNER JOIN [Order Details] AS od
+ON o.OrderID = od.OrderID
+INNER JOIN Employees as e
+ON e.EmployeeID = o.EmployeeID
+WHERE UPPER(CONCAT(e.FirstName, ' ', e.LastName)) = UPPER('ANDREW FULLER')
+AND UPPER(ISNULL(c.Region, 'Sin Region')) = UPPER('Sin Region')
+GROUP BY c.CompanyName, c.Country, c.City, c.Region,CONCAT(e.FirstName, ' ', e.LastName)
+ORDER BY FULLNAME ASC, [Total] DESC;
+
+/*======================================= Manejo de Errores con Try ... Catch ========================================================*/
+
+
+-- SIN TRY - CATCH
+SELECT 10/0;
+
+-- CON TRY .. CATCH
+
+BEGIN TRY
+   SELECT 10/0;
+END TRY
+BEGIN CATCH
+  PRINT 'OCURRIÓ UN ERROR';
+END CATCH;
+GO
+-- EJEMPLO DE USO DE FUNCIONES PARA OBTENER INFORMACIÓN DEL ERROR
+BEGIN TRY
+  SELECT 10/0;
+END TRY  
+BEGIN CATCH
+    PRINT 'Mensaje: ' + ERROR_MESSAGE();
+    PRINT 'Número de Error: ' + CAST(ERROR_NUMBER() AS VARCHAR);
+    PRINT 'Línea de Error: ' + CAST(ERROR_LINE() AS VARCHAR);
+    PRINT 'Estado del Error: ' + CAST(ERROR_STATE() AS VARCHAR);
+END CATCH;
+
+CREATE TABLE clientes(
+   id INT PRIMARY KEY, 
+   nombre VARCHAR(35)
+);
+GO
+
+INSERT INTO clientes 
+VALUES(1,'PANFILO');
+GO
+
+BEGIN TRY
+
+  INSERT INTO clientes 
+  VALUES(1,'EUSTACIO');
+
+END TRY
+BEGIN CATCH
+  PRINT 'ERROR AL INSERTAR: ' + ERROR_MESSAGE();
+  PRINT 'ERROR EN LA LINEA: ' + CAST(ERROR_LINE() AS VARCHAR);
+END CATCH
+GO
+
+BEGIN TRANSACTION;
+
+INSERT INTO clientes 
+VALUES(2,'AMERICO ANGEL');
+
+SELECT * FROM clientes;
+
+COMMIT;
+ROLLBACK;
+
+-- Ejemplo de uso de transacciones junto con el try catch
+
+SELECT * FROM clientes;
+
+BEGIN TRY
+  BEGIN TRANSACTION;
+
+  INSERT INTO clientes 
+  Values(3, 'VALDERABANO');
+  INSERT INTO clientes 
+  VALUES (4, 'ROLES ALINA');
+  
+  COMMIT;
+
+END TRY
+BEGIN CATCH
+  IF @@TRANCOUNT > 1
+  BEGIN
+    ROLLBACK;
+  END
+    PRINT 'Se hizo rollback por error';
+    PRINT 'ERROR: ' + ERROR_MESSAGE();
+END CATCH
+GO
+
+-- CREAR UN STORE PROCEDURE QUE INSERTE UN CLIENTE, CON LAS VALIDACIONES 
+-- NECESARIAS.
+
+CREATE OR ALTER PROCEDURE usp_insertar_cliente
+    @id INT, 
+    @nombre VARCHAR(35)
+AS
+BEGIN
+     
+      BEGIN TRY
+        BEGIN TRANSACTION;
+
+        INSERT INTO clientes 
+        VALUES (@id, @nombre);
+        COMMIT;
+        PRINT 'CLIENTE INSERTADO';
+      END TRY
+      BEGIN CATCH
+          IF @@TRANCOUNT > 1
+            BEGIN
+              ROLLBACK;
+            END
+            PRINT 'ERROR: ' + ERROR_MESSAGE();
+      END CATCH 
+END;
+
+SELECT * FROM clientes;
+
+UPDATE clientes 
+SET nombre = 'AMERICO AZUL'
+WHERE id = 10;
+
+IF @@ROWCOUNT < 1
+BEGIN
+  PRINT @@ROWCOUNT;
+  PRINT 'NO EXISTE EL CLIENTE';
+END
+ELSE
+  PRINT 'CLIENTE ACTUALIZADO';
+
+
+
+  CREATE TABLE teams 
+  (
+    id INT NOT NULL IDENTITY PRIMARY KEY, 
+    nombre NVARCHAR(15)
+
+  );
+
+  SELECT * FROM teams;
+
+  INSERT INTO teams (nombre)
+  VALUES ('CHAFA AZUL');
+
+  -- FORMA DE OBTENER UN IDENTITY INSERTADO FORMA 1
+  DECLARE @id_insertado INT
+  SET @id_insertado = @@IDENTITY
+  PRINT 'ID INSERTADO: ' + CAST(@id_insertado AS VARCHAR);
+  SELECT @id_insertado = @@IDENTITY 
+  PRINT 'ID INSERTADO FORMA 2: ' + CAST(@id_insertado AS VARCHAR);
+
+
+ INSERT INTO teams (nombre)
+  VALUES ('AGUILAS 🦅');
+
+ -- FORMA DE OBTENER UN IDENTITY INSERTADO FORMA 2
+  DECLARE @id_insertado2 INT
+  SET @id_insertado2 = SCOPE_IDENTITY();
+  PRINT 'ID INSERTADO: ' + CAST(@id_insertado2 AS VARCHAR);
+  SELECT @id_insertado2 = SCOPE_IDENTITY(); 
+  PRINT 'ID INSERTADO FORMA 2: ' + CAST(@id_insertado2 AS VARCHAR);
